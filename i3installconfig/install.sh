264169fbf76e5d3610baa46d3f0ad32f19d3c19c
SometimesSquishy <91513394+SometimesSquishy@users.noreply.github.com>
2022-03-31 09:13:22 -0400

    Update install.sh

--- i3installconfig/install.sh
+++ i3installconfig/install.sh
@@ -2,22 +2,22 @@
 
 echo "hope this works"
 sleep 1
-pacman -Syu artix-archlinux-support
-pacman-key --populate archlinux
-pacman -Sy
+sudo pacman -Syu artix-archlinux-support
+sudo pacman-key --populate archlinux
+sudo pacman -Sy
 cp ~/i3installscript/i3installconfig/pacman.conf /etc/pacman.conf
-pacman -Sy
+sudo pacman -Sy
 cd
 git clone https://aur.archlinux.org/yay-git.git
 cd yay-git
 makepkg -si c
 cd
-pacman -Syu
+sudo pacman -Syu
 mkdir ~/Tools
 mkdir ~/Tools/bin
-pacman -S i3-gaps i3blocks i3lock i3status polybar rofi firefox ntfs-3g feh newsboat pulseaudio lxappearance arandr kitty zsh newsboat neovim pulseaudio-alsa alsa-utils picom cmus redshift base-devel curl wget xdotool --no-confirm
+sudo pacman -S i3-gaps i3blocks i3lock i3status polybar rofi firefox ntfs-3g feh newsboat pulseaudio lxappearance arandr kitty zsh newsboat neovim pulseaudio-alsa alsa-utils picom cmus redshift base-devel curl wget xdotool
 
-git clone https://github.com/zdharma-continuum/fast-syntax-highlighting /usr/share/zsh/plugins/fast-syntax-highlighting/
+sudo git clone https://github.com/zdharma-continuum/fast-syntax-highlighting /usr/share/zsh/plugins/fast-syntax-highlighting/
 
 cd
 git clone https://github.com/Waishnav/Watcher
@@ -27,7 +27,7 @@ cd
 yay -S cava backlight_control cbonsai plata-theme pulseaudio-control --no-confirm
 
 cp ~/i3installscript/i3installconfig/scripts/* ~/Tools/bin
-cp ~/i3installscript/i3installconfig/configs ~/.config
+cp ~/i3installscript/i3installconfig/configs/* ~/.config
 cp ~/i3installscript/i3installconfig/zshrc ~/.zshrc
 mkdir .cache zsh
 chmod +x ~/.config/polybar/scripts/*.*
