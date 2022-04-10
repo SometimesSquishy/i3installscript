ff0bdcaa9445d9dddae5d8e1dc514ad77dd6fc05
SometimesSquishy <91513394+SometimesSquishy@users.noreply.github.com>
2022-04-10 17:41:58 -0400

    Update install.sh

--- i3installconfig/install.sh
+++ i3installconfig/install.sh
@@ -3,9 +3,10 @@
 echo "hope this works"
 sleep 1
 sudo pacman -Sy
-sudo cp ~/i3installscript/i3installconfig/pacman.conf /etc/pacman.conf
 sudo pacman -Syu artix-archlinux-support --noconfirm
 sudo pacman-key --populate archlinux --noconfirm
+sudo pacman -Sy
+sudo cp ~/i3installscript/i3installconfig/pacman.conf /etc/pacman.conf
 sudo pacman -Sy
 sudo pacman -Syu --noconfirm
 mkdir ~/Tools
