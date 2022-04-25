da7fc4ff1b55ff01b90eb8061d1801fb986229e7
SometimesSquishy <91513394+SometimesSquishy@users.noreply.github.com>
2022-04-25 09:53:46 -0400

    Update muzix.sh

--- i3installconfig/scripts/muzix.sh
+++ i3installconfig/scripts/muzix.sh
@@ -1,17 +1,18 @@
 
 #muzik
-i3-msg "workspace 3; exec kitty alsamixer"
+
+i3-msg "workspace 2; exec kitty alsamixer"
 sleep 0.2;
 #i3-msg "resize
-i3-msg "workspace 3; flating disable"
-i3-msg "workspace 3; split h"
-i3-msg "workspace 3; exec kitty cmus"
+i3-msg "workspace 2; flating disable"
+i3-msg "workspace 2; split h"
+i3-msg "workspace 2; exec kitty cmus"
 sleep 0.2;
-i3-msg "workspace 3; floating disable"
-i3-msg "workspace 3; split v"
-i3-msg "workspace 3; exec kitty cava"
+i3-msg "workspace 2; floating disable"
+i3-msg "workspace 2; split v"
+i3-msg "workspace 2; exec kitty cava"
 sleep 0.2;
-i3-msg "workspace 3; floating disable"
+i3-msg "workspace 2; floating disable"
 for i in {1..6}
 do
   i3-msg "resize grow left 200 px"
