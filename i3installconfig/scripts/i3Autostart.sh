35c7da368421d68091256fa66a51192cf8eac8e3
SometimesSquishy <91513394+SometimesSquishy@users.noreply.github.com>
2022-04-25 09:53:10 -0400

    Update i3Autostart.sh

--- i3installconfig/scripts/i3Autostart.sh
+++ i3installconfig/scripts/i3Autostart.sh
@@ -15,34 +15,11 @@ i3-msg "workspace 1; split v"
 i3-msg "workspace 1; exec kitty cbonsai -l --life=40 --multiplier=3 -i"
 sleep 0.2;
 
-# Setup Workspace 2
-#i3-msg "workspace 2; floating disable"
-#i3-msg "workspace 2; split h"
-#sleep 0.5;
-
-#Setup Workspace 3
-#i3-msg "workspace 3; exec kitty alsamixer"
-#sleep 0.5;
-#i3-msg "workspace 3; floating disable"
-#i3-msg "workspace 3; split h"
-#i3-msg "workspace 3; exec kitty cmus"
-#sleep 0.5;
-#i3-msg "workspace 3; floating disable"
-#i3-msg "workspace 3; split v"
-#i3-msg "workspace 3; exec kitty cava"
-#sleep 0.5;
-#i3-msg "workspace 3; floating disable"
-#for i in {1..6}
-#do
-#    i3-msg "resize grow left 200 px"
-#    i3-msg "resize shrink up 72 px"
-#done
-#i3-msg "resize grow left 32 px"
-#sleep 0.2;
-
+# Music Setup, Uncomment if dual monitors
+# exec bash ~/Tools/bin/muzix.sh
 
 # Back to Workspace 1
-#i3-msg "workspace 1"
+i3-msg "workspace 1"
 
 #----------------------------------------#
 # General app launch ....                #
